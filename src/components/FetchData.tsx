e90cce38036ba4eece067cbedae377620d7984f9
devLyric <lyricsan@hotmail.com>
2023-11-28 14:33:46 -0300

    adding function to filter countries

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Country } from "../types/Country";
 import CountryCard from "./CountryCard";
+import Filter from "./Filter";
 
 const FetchData = () => {
     const [items, setItems] = useState<Country[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
+    const [selectValue, setSelectValue] = useState<string>("");
+    const [filteredData, setFilteredData] = useState<Country[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
@@ -17,13 +21,38 @@ const FetchData = () => {
             });
     }, []);
 
+    useEffect(() => {
+        const result = items.filter(
+            item =>
+                (!inputValue || item.name.common.toLowerCase().includes(inputValue.toLowerCase())) &&
+                (!selectValue || item.region === selectValue)
+        );
+
+        setFilteredData(result);
+    }, [inputValue, selectValue, items]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectValue(event.target.value);
+    };
+
     return (
         <>
+            <Filter
+                onInputChange={handleInputChange}
+                onSelectChange={handleSelectChange}
+                inputValue={inputValue}
+                selectValue={selectValue}
+            />
+
             <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20">
                 {loading ? (
                     <p className="animate-bounce">Loading...</p>
                 ) : (
-                    items.map(item => (
+                    filteredData.map(item => (
                         <Link key={item.name.common} to={item.cca3.toLowerCase()}>
                             <CountryCard
                                 flag={item.flags.png}
