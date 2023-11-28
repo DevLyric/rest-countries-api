e441a5a3b3ee517cd36e922b2103d0a89967a663
devLyric <lyricsan@hotmail.com>
2023-11-28 13:19:02 -0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CountriesPage from "./pages/CountriesPage";
+import CountryDetails from "./pages/CountryDetails";
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<CountriesPage />} />
+                    <Route path=":name" element={<CountryDetails />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
