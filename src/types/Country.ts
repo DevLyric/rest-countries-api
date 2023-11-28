dd54f8a6f6b966613a796f840aae89caee8e7270
devLyric <lyricsan@hotmail.com>
2023-11-28 15:12:32 -0300

    fix: types

--- src/types/Country.ts
+++ src/types/Country.ts
@@ -1,18 +1,20 @@
 export interface Country {
-    name: {
-        common: string;
-        official: string;
-    };
-    capital: string;
-    population: string;
     flags: {
         svg: string;
         png: string;
     };
-    region: string;
-    languages: [];
-    currencies: [];
-    timezones: string;
-    borders: [];
+    name: {
+        common: string;
+        official: string;
+        nativeName: Record<string, { official: string }>;
+    };
     cca3: string;
+    population: string;
+    region: string;
+    capital: string;
+    subregion: string;
+    tld: string;
+    currencies: Record<string, { name: string }>;
+    languages: Record<string, string>;
+    borders: [];
 }
